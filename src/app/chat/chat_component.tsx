ae65313b8c83c0c0b689e9e30b1f60638bbc956f
sirdesai22 <sirdesai.work@gmail.com>
2025-09-14 02:41:09 +0530

    tested-webllm

---
+++ src/app/chat/chat_component.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { useState } from "react";
+import { MLCEngine } from "@mlc-ai/web-llm";
+import ChatUI from "./chat_ui";
+
+const ChatComponent = () => {
+  const [messages, setMessages] = useState<{ kind: string; text: string }[]>(
+    [],
+  );
+  const [prompt, setPrompt] = useState("");
+  const [runtimeStats, setRuntimeStats] = useState("");
+  const [chat_ui] = useState(new ChatUI(new MLCEngine()));
+  const updateMessage = (kind: string, text: string, append: boolean) => {
+    if (kind == "init") {
+      text = "[System Initalize] " + text;
+    }
+    const msgCopy = [...messages];
+    if (msgCopy.length == 0 || append) {
+      setMessages([...msgCopy, { kind, text }]);
+    } else {
+      msgCopy[msgCopy.length - 1] = { kind, text };
+      setMessages([...msgCopy]);
+    }
+  };
+  return (
+    <div className="flex flex-col items-center">
+      <button
+        className="chatui-btn bg-emerald-500 text-white rounded-md p-2"
+        onClick={() => {
+          chat_ui.asyncInitChat(updateMessage).catch((error) => {
+            console.log(error);
+          });
+        }}
+      >
+        Download Model
+      </button>
+
+      <div className="chatui">
+        <div className="chatui-chat" id="chatui-chat">
+          {messages.map((value, index) => (
+            <div key={index} className={`msg ${value.kind}-msg`}>
+              <div className="msg-bubble">
+                <div className="msg-text">${value.text}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="chatui-inputarea">
+          <input
+            id="chatui-input"
+            type="text"
+            className="chatui-input bg-gray-800 text-white rounded-md p-2"
+            placeholder="Enter your message..."
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                chat_ui
+                  .onGenerate(prompt, updateMessage, setRuntimeStats)
+                  .catch((error) => console.log(error));
+              }
+            }}
+            value={prompt}
+            onChange={(event) => setPrompt(event.target.value)}
+          />
+          <button
+            className="chatui-btn bg-blue-500 text-white rounded-md p-2"
+            onClick={() => {
+              chat_ui
+                .onGenerate(prompt, updateMessage, setRuntimeStats)
+                .catch((error) => console.log(error));
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+
+      <div className="chatui-extra-control">
+        <button
+          className="chatui-btn bg-red-500 text-white rounded-md p-2"
+          onClick={() => {
+            chat_ui
+              .onReset(() => {
+                setMessages([]);
+              })
+              .catch((error) => console.log(error));
+          }}
+        >
+          Reset Chat
+        </button>
+        <label id="chatui-info-label">{runtimeStats}</label>
+      </div>
+    </div>
+  );
+};
+
+export default ChatComponent;
---
+++ src/app/chat/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { CreateMLCEngine } from "@mlc-ai/web-llm";
+import Head from "next/head";
+import ChatComponent from "./chat_component";
+import { Inter } from "next/font/google";
+
+
+type Props = {};
+
+const inter = Inter({ subsets: ["latin"] });
+
+const ChatPage = (props: Props) => {
+  return (
+    <>
+      <Head>
+        <title>Example App</title>
+        <meta
+          name="description"
+          content="Example app for web llm next compatibility"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <ChatComponent />
+      </main>
+    </>
+  );
+};
+
+export default ChatPage;
